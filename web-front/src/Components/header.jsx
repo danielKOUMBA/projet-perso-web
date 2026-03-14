f9894d53c8293bf969ccb4f0d37f7a28e0140844
daniel_prince <danikoumba7@gmail.com>
2026-03-14 11:43:15 +0100

    daniel

--- web-front/src/Components/header.jsx
+++ web-front/src/Components/header.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
 import { UserIcon ,XMarkIcon,Bars3Icon,MagnifyingGlassIcon,BellIcon} from "@heroicons/react/16/solid"
-import logos from '../assets/logos.JPG'
+
 
 
 export default function Header(){
@@ -12,7 +12,7 @@ export default function Header(){
   
   <header>
   
-  <div className="fixed top-0 left-0 w-full z-50 bg-sky-100 flex justify-between items-center p-3 shadow-md">
+  <div className="fixed top-0 left-0 w-full z-50 bg-green-100 flex justify-between items-center p-3 shadow-md">
   
   <div className="flex items-center gap-3">
   
@@ -28,7 +28,7 @@ export default function Header(){
   />
   )}
   
-  <img src={logos} alt="logos" className="w-10 rounded-lg"/>
+  
   
   </div>
   
@@ -43,12 +43,12 @@ export default function Header(){
   </button>
   
   
-  <div className={`absolute right-0 mt-2 bg-white border border-sky-200 rounded-lg shadow-md transition-all duration-300
+  <div className={`absolute right-0 mt-2 bg-white border border-green-200 rounded-lg shadow-md transition-all duration-300
   ${logout ? "opacity-100 scale-100" : "opacity-0 scale-95 pointer-events-none"}
   `}>
   
   <Link to='/'>
-  <p className="px-4 py-2 hover:bg-sky-100 rounded-lg text-center font-semibold">
+  <p className="px-4 py-2 hover:bg-green-100 rounded-lg text-center font-semibold">
   se deconnecter
   </p>
   </Link>
@@ -75,7 +75,7 @@ export default function Header(){
   
   {/* MENU LATERAL */}
   <div
-  className={`fixed top-0 left-0 h-screen w-72 bg-sky-200 z-50 shadow-xl
+  className={`fixed top-0 left-0 h-screen w-72 bg-green-200 z-50 shadow-xl
   transform transition-transform duration-300 ease-out
   ${menu ? "translate-x-0" : "-translate-x-full"}
   `}
@@ -85,23 +85,23 @@ export default function Header(){
   
   <ul className="space-y-3">
   
-  <li className="p-3 rounded-lg hover:bg-sky-100 cursor-pointer font-medium">
+  <li className="p-3 rounded-lg hover:bg-green-100 cursor-pointer font-medium">
   <Link to='/accueil'>Accueil</Link>
   </li>
   
-  <li className="p-3 rounded-lg hover:bg-sky-100 cursor-pointer font-medium">
+  <li className="p-3 rounded-lg hover:bg-green-100 cursor-pointer font-medium">
   <Link to='/commandes'>Gestion des commandes</Link>
   </li>
   
-  <li className="p-3 rounded-lg hover:bg-sky-100 cursor-pointer font-medium">
+  <li className="p-3 rounded-lg hover:bg-green-100 cursor-pointer font-medium">
   <Link to='/tresor'>Gestions des tresoreries</Link>
   </li>
   
-  <li className="p-3 rounded-lg hover:bg-sky-100 cursor-pointer font-medium">
+  <li className="p-3 rounded-lg hover:bg-green-100 cursor-pointer font-medium">
   <Link to='/cout'>Gestions des couts</Link>
   </li>
   
-  <li className="p-3 rounded-lg hover:bg-sky-100 cursor-pointer font-medium">
+  <li className="p-3 rounded-lg hover:bg-green-100 cursor-pointer font-medium">
   <Link to='/chiffre'>Affichage du chiffre d'affaire</Link>
   </li>
   
